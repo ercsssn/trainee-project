9b9b2f4b9aaa9821fcfa5dd09acf4610b9c31d26
John Ericsson Robarios <ercsssn@gmail.com>
2022-03-22 16:44:56 +0800

    Build html structure from scratch
    Tidying things up to improve the readability of my code. This time style sheet is separated.

--- register-page.php
+++ register-page.php
@@ -4,61 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Bootstraplord Sign Up</title>
+    <title>Sign Up Form</title>
     <link rel="stylesheet" href="style.css">
     
 </head>
-<body class="d-flex flex-column justify-content-center align-items-center my-flex-container">
-    <h1>Sign Up</h1>
-    <div class="card text-white bg-dark mb-3 d-flex form-group flex-column" style="width: 500px;">
-      <div class="card-body">
-        <p>Please fill in this form to create an account.</p>
-
-        <label for="email"><b>Email:</b></label>
-        <input type="text" placeholder="Enter Email" name="email" required>
-
-        <label for="psw"><b>Password:</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
-
-        <label for="psw-repeat"><b>Repeat Password:</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-
-        <label for="first-name"><b>First Name:</b></label>
-        <input type="text" placeholder="First Name" name="first-name" required>
-
-        <label for="last-name"><b>Last Name:</b></label>
-        <input type="text" placeholder="Last Name" name="last-name" required>
-
-        <label for="middle-name"><b>Middle Name:</b></label>
-        <input type="text" placeholder="Middle Name" name="middle-name" required>
-
-        <label for="birthday">Birthday:</label>
-        <input type="date" id="birthday" name="birthday" style="color: black;">
-
-        <legend>Choose your gender:</legend>
-        <label for="male">Male</label>
-        <input type="radio" name="gender" id="male" value="male" checked>
-        <label for="female">Female</label>
-        <input type="radio" name="gender" id="female" value="female">
-
-        <label for="bio"><b>Bio:</b></label>
-        <input type="text" placeholder="Bio" name="bio" required>
-
-        <label>
-          <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-        </label>
-
-        <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-
-        <div class="clearfix">
-          <a href="index.html"><button class="cancelbtn">Cancel</button></a>
-         <!-- <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button> Can't finish this now-->
-          <button type="submit" class="signup">Sign Up</button>
-        </div>
-      </div>
-
-    </div>
+<body>
     
 </body>
 </html>
